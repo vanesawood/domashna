71b7f07f63254d21cde79c2cfece88b553b66d70
vanesawood <vanessachriswood@gmail.com>
2024-12-04 23:35:04 +0200

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the length of the train (meters): ");
+        int trainLength = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the speed of the train (m/sec): ");
+        int speed = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the time to pass the bridge (seconds): ");
+        int time = int.Parse(Console.ReadLine());
+
+        int bridgeLength = (speed * time) - trainLength;
+        Console.WriteLine("Length of the bridge: " + bridgeLength + " meters");
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static int CalculateAppleDifference(int k)
+    {
+        int yellowApples = 0;
+        int redApples = 0;
+
+        for (int i = 1; i <= k; i++)
+        {
+            int applesInBox = i * i;
+            if (i % 2 == 0)
+                redApples += applesInBox;
+            else
+                yellowApples += applesInBox;
+        }
+
+        return redApples - yellowApples;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of apple boxes (k): ");
+        int k = int.Parse(Console.ReadLine());
+
+        int result = CalculateAppleDifference(k);
+        Console.WriteLine("Difference between red and yellow apples: " + result);
+    }
+}
