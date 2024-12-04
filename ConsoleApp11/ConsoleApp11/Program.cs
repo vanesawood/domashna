44ab46dc12c47f1cead8f331bee82c3a72158d67
vanesawood <vanessachriswood@gmail.com>
2024-12-04 23:52:30 +0200

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Solution
+{
+    public bool CanLevelUp(int experience, int threshold, int reward)
+    {
+        return experience + reward >= threshold;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+
+        Console.WriteLine(solution.CanLevelUp(10, 15, 5)); 
+        Console.WriteLine(solution.CanLevelUp(10, 15, 4)); 
+    }
+}
